6ea304261ac2be5c5acef2e7bfc3afbc7847afc8
cyber-chalk <luka.chalker@gmail.com>
2023-11-06 23:59:29 +1100

    almost grass

--- public/script.js
+++ public/script.js
@@ -1,5 +1,26 @@
-function place (num, squares) {
-    
+function place(rows, columns) {
+	for (let i = 0; i < columns; i++) {
+		for (let n = 0; n < rows; n++) {
+			let image = document.createElement("img");
+			image.src = "./images/grasstiles.png";
+			document.getElementById("container").append(image);
+			image.style.gridRow = n + 1;
+			image.style.gridColumn = i + 1;
+		}
+	}
 }
+// repositon
+place(
+	window
+		.getComputedStyle(document.getElementById("container"))
+		.getPropertyValue("grid-template-rows")
+		.split(" ").length,
+	window
+		.getComputedStyle(document.getElementById("container"))
+		.getPropertyValue("grid-template-columns")
+		.split(" ").length
+);
 
-place ()
+// looping system
+//coords sytem / normal
+// other loop O(n)
