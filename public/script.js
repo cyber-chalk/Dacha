1d34d857dd9c94c985d28b62b1a8d184d33d0a45
luka.chalker <52194915+cyber-chalk@users.noreply.github.com>
2023-11-07 10:36:45 +0000

    sizing and

--- public/script.js
+++ public/script.js
@@ -6,9 +6,20 @@ function place(rows, columns) {
 			document.getElementById("container").append(image);
 			image.style.gridRow = n + 1;
 			image.style.gridColumn = i + 1;
+			image.style = "width: 128px; height: 128px;";
+			image.style.objectFit = "cover";
+			// image.style.objectPosition = "-200px 0";
+			image.style.objectPosition = `${
+				Math.floor(getRandom(0, 6)) * -128 - 256
+			}px 0`;
 		}
 	}
 }
+
+function getRandom(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
 // repositon
 place(
 	window
